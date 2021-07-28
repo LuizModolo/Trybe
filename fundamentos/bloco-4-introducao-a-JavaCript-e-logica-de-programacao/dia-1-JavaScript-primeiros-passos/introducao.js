ca147121c00decd7a735a75042533b7a3b7490b4
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-07-28 16:44:27 -0300

    inicio JS branch nova

---
+++ fundamentos/bloco-4-introducao-a-JavaCript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/introducao.js
@@ -0,0 +1,27 @@
+const myName = "Luiz Fernando Christianini Módolo";
+const birthCity = Campinas;
+birthCity = Americana;
+let birthYear = 1988;
+birthYear = 2030
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+console.log(birthYear);
+
+console.log(birthCity);
+
+let patientId = 50;
+let isEnrolled = true;
+const patientInfo = {
+  firstName: 'Ana',
+  lastName: 'Santos',
+};
+const patientEmail = 'ana@email.com';
+
+console.log(typeof patientId);
+console.log(typeof isEnrolled);
+console.log(typeof patientInfo);
+console.log(typeof patientEmail);
+console.log(typeof patientAge);
