0a718481e3f659fe60d5430f6c157005c86b4274
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-07-29 12:13:23 -0300

    teste

---
+++ fundamentos/bloco-4-introducao-a-JavaCript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio1.js
@@ -0,0 +1,8 @@
+let numA = 7;
+let numB = 6;
+
+console.log (numA + numB);
+console.log (numA - numB);
+console.log (numA * numB);
+console.log (numA / numB);
+console.log (numA % numB);
---
+++ fundamentos/bloco-4-introducao-a-JavaCript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio2.js
@@ -0,0 +1,12 @@
+let numA = 77
+let numB = 55
+numA = 32
+numB = 32
+
+if (numA > numB){
+    console.log (numA);
+} else if (numB > numA) {
+    console.log (numB);
+} else {
+    console.log (numA);
+}
