d011cca63e99c521f869e11d18112f19d250c706
Luiz Fernando C. Modolo <modolo.luizfernando@gmail.com>
2021-07-29 21:38:38 -0300

    exercicios 8 a 11

---
+++ fundamentos/bloco-4-introducao-a-JavaCript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio8.js
@@ -0,0 +1,11 @@
+let numA = 77
+let numB = 55
+let numC = 70
+numC = 71
+numB = 24
+
+if (numA % 2 == 0 || numB % 2 == 0 || numC % 2 == 0){
+    console.log ('true');
+} else {
+    console.log ('false');
+}
---
+++ fundamentos/bloco-4-introducao-a-JavaCript-e-logica-de-programacao/dia-1-JavaScript-primeiros-passos/exercicio9.js
@@ -0,0 +1,10 @@
+let numA = 78
+let numB = 52
+let numC = 70
+numA = 77
+
+if (numA % 2 !== 0 || numB % 2 !== 0 || numC % 2 !== 0){
+    console.log ('true');
+} else {
+    console.log ('false');
+}
